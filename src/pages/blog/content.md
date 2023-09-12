828c5e02622d29e705deb67577a5bdeb95016635
karlosvas <126987511+karlosvas@users.noreply.github.com>
2023-09-12 22:06:36 +0200

    End of the first class

---
+++ src/pages/blog/content.md
@@ -0,0 +1,19 @@
+---
+title: 'Contenido de la pagina'
+layout: "../../layouts/Layout.astro"
+---
+# Esto es un ejemplo de articulo
+
+lorem ipum jdifbhiosdoifh bshiofhiosdcvhjcvkhnsklcshjkcklm f ibshicvb
+
+## Subtitulo
+
+Lorem  jsfhsgdfih f isfbisbfihs fhk
+
+### Subtitulo 3
+
+```javascript
+function (){
+    console.log("karlosvas estuvo aqui")
+}
+```
---
+++ src/services/spacex.ts
@@ -0,0 +1,27 @@
+import { type APISpaceXResponse, type Doc } from "../types/api.js"
+
+export const getLaunchBy = async ({ id }: { id: string }) => {
+    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+
+    const launch = (await res.json()) as Doc
+    return launch
+}
+
+export const getLatestLaunches = async () => {
+    const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+        method: "POST",
+        headers: {
+            "Content-Type": "aplications/json"
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                },
+                limit: 12
+            },
+        }),
+    })
+    const { docs: launches } = (await res.json()) as APISpaceXResponse
+    return launches
+}
